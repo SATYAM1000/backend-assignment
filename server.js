de6232e1bb602656fe7879ad6cbe3569594ded2f
Satyam Kumar <satyamsingh748846@gmail.com>
2024-02-15 20:24:45 +0530

    new

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 const app = express();
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+  });
 app.use(express.json());
 app.use(cors());
 const httpServer = createServer(app);
